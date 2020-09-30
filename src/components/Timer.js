bf944e1fec8451d5c393c6f3b750f2266d2f7895
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2020-09-30 09:28:09 +0700

    add jquery

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import moment from 'moment';
 import momentDurationFormatSetup from 'moment-duration-format';
+import $ from 'jquery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faStop, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -13,9 +14,17 @@ function Timer({
     handleStartStopClick,
     handleResetButtonClick
 }) {
-    // Change timerLeft whenever sessionLength changes
+    useEffect(() => {
+        if (timeLeft < 60) {
+            $('#time-left').css('color', 'red');
+            $('#timer-label').css('color', 'red');
+        } else {
+            $('#time-left').css('color', 'white');
+            $('#timer-label').css('color', 'white');
+        }
+    });
 
-    const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss', { trim: false})
+    const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss', { trim: false });
 
     return (
         <div>
